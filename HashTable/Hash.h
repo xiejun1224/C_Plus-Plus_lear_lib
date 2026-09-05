bea052275c109588e73958b86df1251707441545
xiejun1224 <xiejun1224@users.noreply.github.com>
2026-09-05 15:32:38 +0800

    fix: 保留原始注释(BKDR/const说明/参考代码)

--- HashTable/Hash.h
+++ HashTable/Hash.h
@@ -27,21 +27,23 @@ struct Hash
         return (size_t)key;
     }
 };
-template<>
-struct Hash<string>
-{
+ template<>
+ struct Hash<string>
+ {
     size_t operator()(const string& st)const
     {
         size_t hashi=0;
         for(const auto& e : st)
         {
             hashi=hashi*131+e;
+            //BKDR算法防止碰撞
         }
+
         return hashi;
     }
-};
+ };
 
-template<class K,class V,class hash=Hash<K>>
+template<class K,class V ,class hash=Hash<K>>
 class HashTable
 {
 public:
@@ -51,6 +53,7 @@ public:
 
     size_t Hash_Pos(const size_t& key) const
     {
+        //const函数不能调用非const函数所以扰动函数要加const
         size_t hashi=Disturb(key);
         return hashi&(_table.size()-1);
     }
@@ -68,8 +71,23 @@ public:
         {
             return false;
         }
+        //储存的数据大于0.7时扩容
+        //if(_n*10/_table.size()>=7)
+        //改成乘法避免除法的开销
         if(_n*10>=7*_table.size())
-        {
+        {   
+        //内存消耗过大 
+            // HashTable newtable;
+            // newtable._table.resize(2*_table.size());
+            // newtable._m+=1+_m;
+            // for(const auto& e: _table)
+            // {
+            //     if(e._status==EXIST)
+            //     newtable.Insert(e._kv);
+            // }
+            // _n=newtable._n;
+            // _m=newtable._m;
+            // swap(_table,newtable._table);
             size_t newM=_m+1;
             size_t newsize=size_t(1)<<newM;
             vector<HashData<K,V>> newtable(newsize);
@@ -90,17 +108,18 @@ public:
             swap(newtable,_table);
             _m=newM;
         }
-        hash _hash;
-        size_t hashi=Hash_Pos(_hash(kv.first));
-        while(_table[hashi]._status==EXIST)
-        {
-            hashi=(hashi+1)&(_table.size()-1);
-        }
-        _table[hashi]._kv=kv;
-        _table[hashi]._status=EXIST;
-        ++_n;
-        return true;
+        //x % y == x&(y-1)，例如x%16==保留0-15，按位于15也等于保留0-15
+    hash _hash;
+    size_t hashi=Hash_Pos(_hash(kv.first));
+    while(_table[hashi]._status==EXIST)
+    {
+        hashi=(hashi+1)&(_table.size()-1);
     }
+    _table[hashi]._kv=kv;
+    _table[hashi]._status=EXIST;
+    ++_n;
+    return true;
+    }    
 
     HashData<K,V>* Find(const K& key)
     {
@@ -117,9 +136,10 @@ public:
             if(hashi==start)
                 break;
         }
+
         return nullptr;
     }
-    const HashData<K,V>* Find(const K& key) const
+     const HashData<K,V>* Find(const K& key) const
     {
         hash _hash;
         size_t hashi=Hash_Pos(_hash(key));
@@ -134,6 +154,7 @@ public:
             if(hashi==start)
                 break;
         }
+
         return nullptr;
     }
     bool Erase(const K& key)
